5d91ad7edcff3eab3c6d0bf5ddd06a7a75fba6c0
unknown <hussam.ulhaq@yahoo.com>
2023-07-14 12:17:43 +0500

    Includes comparison bw promises and callbacks; included different promises' methods

--- promises.js
+++ promises.js
@@ -13,7 +13,7 @@ p.then((message)=>{
     console.log('We are in the catch '+ message)
 })
 
-// rewrite to memorize syntax
+// rewrite twice to memorize syntax
 
 let p2 = new Promise((resolve, reject) => {
     let b =8
@@ -48,3 +48,119 @@ p3.then((message)=>{
 
 
 // callbacks versus promises
+
+
+function errorCheck(){
+
+}
+
+// Promises verus callback
+
+// Handling via callbacks
+const isHot = 1;
+const isRaining = true;
+
+function weatherCheck(callback, errorCallback) {
+    if (typeof isHot !== 'boolean' || typeof isRaining !== 'boolean') {
+        let message = {
+            name: 'Type',
+            message: 'Error'
+        };
+        errorCallback(message);
+    } else if (isHot && isRaining) {
+        let message = 'It is humid';
+        callback(message);
+    } else if (!isHot && isRaining) {
+        let message = 'It is cold weather';
+        callback(message);
+    } else if (!isHot && !isRaining) {
+        let message = 'It is pleasant weather';
+        callback(message);
+    } else if (isHot && !isRaining) {
+        let message = 'It is hot weather';
+        callback(message);
+    }
+}
+
+weatherCheck(
+    (message) => {
+        console.log(message);
+    },
+    (message) => {
+        console.log(message.name + message.message);
+    }
+);
+
+// Handling via promises
+
+const isHotPromise = false;
+const isRainingPromise = true;
+
+function weatherCheckPromises() {
+    return new Promise((resolve, reject) => {
+        if (typeof isHotPromise !== 'boolean' || typeof isRainingPromise !== 'boolean') {
+            let message = {
+                name: 'Type',
+                message: 'Error'
+            };
+            reject(message);
+        } else if (isHotPromise && isRainingPromise) {
+            let message = 'It is humid';
+            resolve(message);
+        } else if (!isHotPromise && isRainingPromise) {
+            let message = 'It is cold weather';
+            resolve(message);
+        } else if (!isHotPromise && !isRainingPromise) {
+            let message = 'It is pleasant weather';
+            resolve(message);
+        } else if (isHotPromise && !isRainingPromise) {
+            let message = 'It is hot weather';
+            resolve(message);
+        }
+        
+    })
+
+}
+
+weatherCheckPromises().then((message)=>{
+    console.log(message)
+}).catch((message)=>{
+    console.log(message.name+message.message)
+})
+
+
+// promises' methods
+
+const downloadFileOne = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+        resolve('Downloaded file one')
+
+    }, 2000)
+    
+})
+
+const downloadFileTwo = new Promise((resolve, reject) => {
+    resolve('Downloaded file two')
+})
+
+const downloadFileThree = new Promise((resolve, reject) => {
+    resolve('Downloaded file three')
+})
+
+// returns a message as soon as the first promise resolves
+Promise.race([
+    downloadFileOne,
+    downloadFileTwo,
+    downloadFileThree
+]).then((message)=>{
+    console.log(message)
+})
+
+// waits for all the promises to get resolved and returns an array of messages
+Promise.all([
+    downloadFileOne,
+    downloadFileTwo,
+    downloadFileThree
+]).then((messages)=>{
+    console.log(messages)
+})
