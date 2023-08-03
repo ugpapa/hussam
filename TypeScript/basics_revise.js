bc388c62d8116f7dee3ac26776f5aa71fad8b310
hussam95 <hussam.ulhaq@yahoo.com>
2023-08-03 12:05:56 +0500

    clean working dir

--- TypeScript/basics_revise.js
+++ TypeScript/basics_revise.js
@@ -14,6 +14,45 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var __esDecorate = (this && this.__esDecorate) || function (ctor, descriptorIn, decorators, contextIn, initializers, extraInitializers) {
+    function accept(f) { if (f !== void 0 && typeof f !== "function") throw new TypeError("Function expected"); return f; }
+    var kind = contextIn.kind, key = kind === "getter" ? "get" : kind === "setter" ? "set" : "value";
+    var target = !descriptorIn && ctor ? contextIn["static"] ? ctor : ctor.prototype : null;
+    var descriptor = descriptorIn || (target ? Object.getOwnPropertyDescriptor(target, contextIn.name) : {});
+    var _, done = false;
+    for (var i = decorators.length - 1; i >= 0; i--) {
+        var context = {};
+        for (var p in contextIn) context[p] = p === "access" ? {} : contextIn[p];
+        for (var p in contextIn.access) context.access[p] = contextIn.access[p];
+        context.addInitializer = function (f) { if (done) throw new TypeError("Cannot add initializers after decoration has completed"); extraInitializers.push(accept(f || null)); };
+        var result = (0, decorators[i])(kind === "accessor" ? { get: descriptor.get, set: descriptor.set } : descriptor[key], context);
+        if (kind === "accessor") {
+            if (result === void 0) continue;
+            if (result === null || typeof result !== "object") throw new TypeError("Object expected");
+            if (_ = accept(result.get)) descriptor.get = _;
+            if (_ = accept(result.set)) descriptor.set = _;
+            if (_ = accept(result.init)) initializers.unshift(_);
+        }
+        else if (_ = accept(result)) {
+            if (kind === "field") initializers.unshift(_);
+            else descriptor[key] = _;
+        }
+    }
+    if (target) Object.defineProperty(target, contextIn.name, descriptor);
+    done = true;
+};
+var __runInitializers = (this && this.__runInitializers) || function (thisArg, initializers, value) {
+    var useValue = arguments.length > 2;
+    for (var i = 0; i < initializers.length; i++) {
+        value = useValue ? initializers[i].call(thisArg, value) : initializers[i].call(thisArg);
+    }
+    return useValue ? value : void 0;
+};
+var __setFunctionName = (this && this.__setFunctionName) || function (f, name, prefix) {
+    if (typeof name === "symbol") name = name.description ? "[".concat(name.description, "]") : "";
+    return Object.defineProperty(f, "name", { configurable: true, value: prefix ? "".concat(prefix, " ", name) : name });
+};
+var _this = this;
 // Variable type annotations
 var myString = "Hello World";
 var myNumber = 4;
@@ -285,3 +324,28 @@ function rectArea(rect) {
     return (rect.y.x - rect.x.x) * (rect.y.y - rect.x.y);
 }
 console.log(rectArea(rect).toFixed(2));
+// 10. Decorators: allow to add metadata to a class, method, accessor, property or parameter
+function logClass(target) {
+    console.log("Class: ".concat(target.name));
+    return target;
+}
+var MyClass = function () {
+    var _classDecorators = [logClass];
+    var _classDescriptor;
+    var _classExtraInitializers = [];
+    var _classThis;
+    var MyClass = _classThis = /** @class */ (function () {
+        function MyClass_1() {
+            console.log("Constructor");
+        }
+        return MyClass_1;
+    }());
+    __setFunctionName(_classThis, "MyClass");
+    (function () {
+        __esDecorate(null, _classDescriptor = { value: _classThis }, _classDecorators, { kind: "class", name: _classThis.name }, null, _classExtraInitializers);
+        MyClass = _classThis = _classDescriptor.value;
+        __runInitializers(_classThis, _classExtraInitializers);
+    })();
+    return MyClass = _classThis;
+}();
+new MyClass();
